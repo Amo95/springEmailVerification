d27f7a428dec131fd2b1f574b07254e19e12a0eb
amo95 <jameaamo@gmail.com>
2023-12-29 01:14:30 +0000

    removed mail server password

--- src/main/java/com/services/emailverification/EmailVerificationApplication.java
+++ src/main/java/com/services/emailverification/EmailVerificationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmailVerificationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmailVerificationApplication.class, args);
     }
